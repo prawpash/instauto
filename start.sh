4e835232e7cafd2ba7b665424e195a72dda5c15b
prawira <robbyprawira3@gmail.com>
2022-01-27 11:53:09 +0700

    📝 Update Start File

--- start.sh
+++ start.sh
@@ -2,11 +2,14 @@
 configFilePath="/media/config.json"
 userMustFollowPath="/media/userMustFollow.csv"
 commentsFilePath="/media/comments.csv"
+userNeedToFollows="/media/userNeedToFollows.csv"
 
 [ ! -f "${configFilePath}" ] && cp "./config.json" "${configFilePath}"
 
 [ ! -f $userMustFollowPath ] && echo "username" >> $userMustFollowPath
 
 [ ! -f $commentsFilePath ] && echo "comments" >> $commentsFilePath
+
+[ ! -f $userNeedToFollows ] && echo "username" >> $userNeedToFollows
 
 yarn start
